5ba3859d941c262cae483dc12217e18118866979
leeedylson19 <lee.edylson19@gmail.com>
2022-06-12 23:35:19 -0500

    first commit

---
+++ INFRAESTRUCTURE/Migrations/202206080239549_v2.cs
@@ -0,0 +1,27 @@
+﻿namespace INFRAESTRUCTURE.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v2 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        stundeID = c.Int(nullable: false, identity: true),
+                        studentName = c.String(nullable: false),
+                        studentAddress = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.stundeID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+        }
+    }
+}
---
+++ MVCAjax/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Linq;
+using DOMAIN;
+using SERVICE;
+using MVCAjax.Models;
+
+namespace MVCAjax.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentService service = new StudentService();
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             ID = c.studentID,
+                             Address=c.studentAddress,
+                             Name=c.studentName
+                         }).ToList();
+            return View(model);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult getStudent(string id)
+        {
+            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult createStudent(Student std)
+        {
+            service.Insert(std);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }        
+    }
+}
